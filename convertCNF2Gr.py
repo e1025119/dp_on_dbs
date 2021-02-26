e6091309f0dd7d11825869c00de7e0a32759b4f0
Christoph <e1025119@student.tuwien.ac.at>
2021-02-26 15:26:05 +0100

    changes to convertCNF2GR.py - changes number of nodes in the .gr file to the number of nodes given in the .cnf file instead of the automatically determined one.

--- convertCNF2Gr.py
+++ convertCNF2Gr.py
@@ -17,16 +17,32 @@ else:
 if args.output_file:
     output_file = args.output_file
 else:
-    output_file = input_file.split(".")[0]+"_new.gr"
-
-with open(input_file, "r") as f_in:
-    header = f_in.readline()
-    while header.split()[0] != "p":
-        header = f_in.readline()
-    num = header.split()[2]
+    output_file = input_file.split(".")[0]+".gr"
 
 g = Graph.from_file(input_file)
 #g.write_gr(sys.stdout) #open(sys.stdout, "w"))
 f_out = open(output_file, "w")
 g.write_gr(f_out, False) #open(sys.stdout, "w"))
+
+# replace number of nodes in .gr with the number from .cnf
+with open(input_file, "r") as f_in:
+    header = f_in.readline()
+    while header.split()[0] != "p":
+        header = f_in.readline()
+    num_cnf = header.split()[2]
+
+with open(output_file, "r+") as f_out:
+    out_all = f_out.read().split("\n")    
+    for idx, line in enumerate(out_all):
+        lline = line.split()
+        if lline[0] == "p":
+            lline[2] = num_cnf
+            line = " ".join(lline)
+            out_all[idx] = line
+            break
+    f_out.seek(0)
+    f_out.truncate()
+    f_out.write("\n".join(out_all))
+
+
 
